fa04ebd77894718df4685fdba448d71676ad056d
1Nix1 <thiagoaugustodeoliveiraalves@gmail.com>
2020-11-20 18:04:20 -0300

    config

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+    
+    </div>
+    <!-- .container -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" crossorigin="anonymous">
+
+    <title>Pay to on</title>
+  </head>
+  <body>
+    <div class="container">
+
+        <div class="jumbotron bd-danger">
+            <h1>Pay to on</h1>
+            <p>Teste de página</p>
+        </div>
+    
+
+    
