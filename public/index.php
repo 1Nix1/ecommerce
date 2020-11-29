4c5b4fcd85085ff39e7e6bec174ce8698a9e0067
1Nix1 <thiagoaugustodeoliveiraalves@gmail.com>
2020-11-29 16:20:25 -0300

    arquivos antigos excluidos

--- public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include __DIR__.'/../includes/header.php';
-include __DIR__.'/../includes/aside.php';
-include __DIR__.'/../includes/content.php';
-include __DIR__.'/../includes/footer.php';
